1220f5d54d8d5d25531209b30b82221dd9fb85ff
amidman <mcdvaran@gmail.com>
2024-12-09 19:14:43 +0400

    add QueryMap func

--- batch.go
+++ batch.go
@@ -7,7 +7,7 @@ import (
 
 func Query(template string, rowsCount, argsPerRow uint64) string {
 	if rowsCount == 0 || argsPerRow == 0 {
-		return "()"
+		return fmt.Sprintf(template, "()")
 	}
 
 	buf := strings.Builder{}
@@ -18,6 +18,47 @@ func Query(template string, rowsCount, argsPerRow uint64) string {
 
 		for i := range argsPerRow {
 			buf.WriteString(fmt.Sprintf("$%d", index+i+1))
+
+			if i < argsPerRow-1 {
+				buf.WriteByte(',')
+			}
+		}
+
+		buf.WriteString(")")
+
+		if i < rowsCount-1 {
+			buf.WriteByte(',')
+		}
+	}
+
+	return fmt.Sprintf(template, buf.String())
+}
+
+type MapFunc func(argNumber uint64) string
+
+func QueryMap(template string, rowsCount, argsPerRow uint64, argMapper map[uint64]MapFunc) string {
+	if rowsCount == 0 || argsPerRow == 0 {
+		return fmt.Sprintf(template, "()")
+	}
+
+	if argMapper == nil {
+		argMapper = make(map[uint64]MapFunc)
+	}
+
+	buf := strings.Builder{}
+
+	for i := range rowsCount {
+		buf.WriteString("(")
+		index := i * argsPerRow
+
+		for i := range argsPerRow {
+			argNumber := index + i + 1
+			switch f := argMapper[i]; f {
+			case nil:
+				buf.WriteString(fmt.Sprintf("$%d", argNumber))
+			default:
+				buf.WriteString(f(argNumber))
+			}
 
 			if i < argsPerRow-1 {
 				buf.WriteByte(',')
--- batch_test.go
+++ batch_test.go
@@ -1,6 +1,7 @@
 package batch_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/amidgo/batch"
@@ -30,6 +31,20 @@ func (q *QueryTest) Test(t *testing.T) {
 func Test_Query(t *testing.T) {
 	tester.RunNamedTesters(t,
 		&QueryTest{
+			CaseName:      "zero rows count",
+			RowsCount:     0,
+			ArgPerRow:     2,
+			Template:      "INSERT INTO users (id, name) VALUES %s",
+			ExpectedQuery: "INSERT INTO users (id, name) VALUES ()",
+		},
+		&QueryTest{
+			CaseName:      "zero args count",
+			RowsCount:     2,
+			ArgPerRow:     0,
+			Template:      "INSERT INTO users (id, name) VALUES %s",
+			ExpectedQuery: "INSERT INTO users (id, name) VALUES ()",
+		},
+		&QueryTest{
 			CaseName:      "single row",
 			RowsCount:     1,
 			ArgPerRow:     2,
@@ -42,6 +57,72 @@ func Test_Query(t *testing.T) {
 			ArgPerRow:     2,
 			Template:      "INSERT INTO users (id, name) VALUES %s",
 			ExpectedQuery: "INSERT INTO users (id, name) VALUES ($1,$2),($3,$4),($5,$6)",
+		},
+	)
+}
+
+type QueryMapTest struct {
+	CaseName      string
+	Template      string
+	RowsCount     uint64
+	ArgPerRow     uint64
+	ExpectedQuery string
+	ArgsMap       map[uint64]batch.MapFunc
+}
+
+func (q *QueryMapTest) Name() string {
+	return q.CaseName
+}
+
+func (q *QueryMapTest) Test(t *testing.T) {
+	actualQuery := batch.QueryMap(q.Template, q.RowsCount, q.ArgPerRow, q.ArgsMap)
+
+	if q.ExpectedQuery != actualQuery {
+		t.Fatalf("query not equal, expected %s, actual %s", q.ExpectedQuery, actualQuery)
+	}
+}
+
+func Test_QueryMap(t *testing.T) {
+	tester.RunNamedTesters(t,
+		&QueryMapTest{
+			CaseName:      "zero rows count",
+			RowsCount:     0,
+			ArgPerRow:     2,
+			Template:      "INSERT INTO users (id, name) VALUES %s",
+			ExpectedQuery: "INSERT INTO users (id, name) VALUES ()",
+		},
+		&QueryMapTest{
+			CaseName:      "zero args count",
+			RowsCount:     2,
+			ArgPerRow:     0,
+			Template:      "INSERT INTO users (id, name) VALUES %s",
+			ExpectedQuery: "INSERT INTO users (id, name) VALUES ()",
+		},
+		&QueryMapTest{
+			CaseName:      "single row",
+			RowsCount:     1,
+			ArgPerRow:     2,
+			Template:      "INSERT INTO users (id, name) VALUES %s",
+			ExpectedQuery: "INSERT INTO users (id, name) VALUES ($1,$2)",
+		},
+		&QueryMapTest{
+			CaseName:      "many rows",
+			RowsCount:     3,
+			ArgPerRow:     2,
+			Template:      "INSERT INTO users (id, name) VALUES %s",
+			ExpectedQuery: "INSERT INTO users (id, name) VALUES ($1,$2),($3,$4),($5,$6)",
+		},
+		&QueryMapTest{
+			CaseName:  "map",
+			RowsCount: 3,
+			ArgPerRow: 3,
+			Template:  "INSERT INTO users (id, name, toy_id) VALUES %s",
+			ArgsMap: map[uint64]batch.MapFunc{
+				2: func(argNumber uint64) string {
+					return fmt.Sprintf("(SELECT id FROM toys WHERE name = $%d)", argNumber)
+				},
+			},
+			ExpectedQuery: "INSERT INTO users (id, name, toy_id) VALUES ($1,$2,(SELECT id FROM toys WHERE name = $3)),($4,$5,(SELECT id FROM toys WHERE name = $6)),($7,$8,(SELECT id FROM toys WHERE name = $9))",
 		},
 	)
 }
